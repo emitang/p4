77cc3299925b20e4f29eec3bd32f370216ed80a3
Emily Tang <emitang@mit.edu>
2014-12-10 21:16:49 -0500

    first commit

---
+++ app/views/base.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title','Bar Buddy')</title>
+	<link rel='stylesheet' href='css/p4.css' type='text/css' />
+
+	@yield('head')
+
+</head>
+<body>
+	@yield('content')
+
+	@yield('body')
+
+
+</body>
+</html>
---
+++ app/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('base')
+
+@section('content')
+
+<div class="body-wrapper">
+	<h1>Bar Buddy</h1>
+
+</div>
+
+@stop
